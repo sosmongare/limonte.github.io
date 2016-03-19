50a47cf4e95d893e40d1695bd10d450579f0bd60
Limon Monte <limon.monte@gmail.com>
2016-03-19 19:06:17 +0100

    Add jQuery (temporary, I hope:)

--- js/symmetry.js
+++ js/symmetry.js
@@ -1,31 +1,28 @@
-(function() {
+$(window).load(function() {
   var width = 1000;
 
-  // start
-  window.onload = function() {
-    var container = document.getElementById('svg');
-    var svg = container.contentDocument;
+  var container = document.getElementById('svg');
+  var svg = container.contentDocument;
 
-    var symmetrySource = svg.getElementById('symmetry-source');
-    var symmetryTarget = svg.getElementById('symmetry-target');
-    var paths = symmetrySource.getElementsByTagName('path');
+  var symmetrySource = svg.getElementById('symmetry-source');
+  var symmetryTarget = svg.getElementById('symmetry-target');
+  var paths = symmetrySource.getElementsByTagName('path');
 
-    for (var i = 0; i < paths.length; i++) {
-      var original = paths[i];
-      var flipped = original.cloneNode(true);
+  for (var i = 0; i < paths.length; i++) {
+    var original = paths[i];
+    var flipped = original.cloneNode(true);
 
-      // flip curve
-      var d = flipped.getAttribute('d');
-      var points = d.split(' ');
-      for (var j = 0; j < points.length; j++) {
-        var x = points[j].match(/(\d+),/)[1];
-        x = width - x;
-        points[j] = points[j].replace(/\d+,/, x + ',');
-      }
-      d = points.join(' ');
-      flipped.setAttribute('d', d)
-
-      symmetryTarget.appendChild(flipped);
+    // flip curve
+    var d = flipped.getAttribute('d');
+    var points = d.split(' ');
+    for (var j = 0; j < points.length; j++) {
+      var x = points[j].match(/(\d+),/)[1];
+      x = width - x;
+      points[j] = points[j].replace(/\d+,/, x + ',');
     }
+    d = points.join(' ');
+    flipped.setAttribute('d', d)
+
+    symmetryTarget.appendChild(flipped);
   }
-})();
+});
