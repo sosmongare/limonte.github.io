87b7542e81cb2614ae0fe4367fefa3383f9f5235
Limon Monte <limon.monte@gmail.com>
2016-08-03 00:55:41 +0200

    Get rid of jQuery

--- js/app.js
+++ js/app.js
@@ -1,82 +1,164 @@
-var data = {
-  sound: localStorage.getItem('sound')? JSON.parse(localStorage.getItem('sound')) : true,
-  backgroundMusic: new Audio('/mp3/The_Commission_Creton.mp3')
-};
+'use strict';
 
-var vm = new Vue({
-  el: '#app',
-  data: data,
-  created: function() {
-    this.backgroundMusic.volume = 0.5;
-    this.backgroundMusic.loop = true;
-    if (this.sound) {
-      this.backgroundMusic.play();
-    }
-  }
-});
+document.addEventListener('DOMContentLoaded', function() {
+  var data = {
+    sound: localStorage.getItem('sound')? JSON.parse(localStorage.getItem('sound')) : true,
+    backgroundMusic: new Audio('/mp3/The_Commission_Creton.mp3')
+  };
 
-vm.$watch('sound', function(sound) {
-  if (sound) {
-    this.backgroundMusic.play();
-  } else {
-    this.backgroundMusic.pause();
-  }
-  localStorage.setItem('sound', sound);
-});
-
-$(window).on('load', function() {
-  var svg = $('#svg').contents();
-  var zippi = new Audio('/mp3/zippi.mp3');
-  zippi.volume = 0.3;
-
-  svg.on('click touchstart', '.skill:not(.current)', function() {
-    svg.find('.skill').removeClass('current');
-    $(this).addClass('active current');
-
-    // detect box dimensions
-    $('.skillBox').css('left', -999);
-    var skillName = $(this).attr('id');
-    var box = $('.skillBox.' + skillName);
-    var boxWidth = box.width();
-    var boxHeight = box.height();
-
-    // set initial dimensions for animation
-    box.css({
-      width: 0,
-      height: 3,
-      left: '50%'
-    });
-
-    box.animate(
-      // first, animate box horizontally
-      {width: boxWidth}, 150,
-      function() {
-        // then vertically
-        $(this).animate({height: boxHeight}, 150);
+  var vm = new Vue({
+    el: '#app',
+    data: data,
+    created: function() {
+      this.backgroundMusic.volume = 0.5;
+      this.backgroundMusic.loop = true;
+      if (this.sound) {
+        this.backgroundMusic.play();
       }
-    );
-
-    if (data.sound) {
-      zippi.play();
     }
   });
 
-  // initial hightlight brain triangles, little bit of recursion here :)
-  var brainParts = svg.find('.skill');
-  var highlightBrainParts = function(depth) {
-    if (depth > 20) {
+  vm.$watch('sound', function(sound) {
+    if (sound) {
+      this.backgroundMusic.play();
+    } else {
+      this.backgroundMusic.pause();
+    }
+    localStorage.setItem('sound', sound);
+  });
+
+  var zippi = new Audio('/mp3/zippi.mp3');
+  zippi.volume = 0.3;
+
+  var skillBoxes = document.getElementsByClassName('skillBox');
+
+  var svg = document.getElementById('svg');
+  svg.addEventListener('load', function() {
+    symmetry(svg);
+    var svgDoc = svg.contentDocument;
+
+    svgDoc.onclick = function(e) {
+      var target = e.target;
+      if (target.classList.contains('skill') && !target.classList.contains('current')) {
+        var current = svgDoc.getElementsByClassName('current');
+        if (current.length) {
+          current[0].classList.remove('current');
+        }
+
+        target.classList.add('active');
+        target.classList.add('current');
+
+        var skillName = target.getAttribute('id');
+        Array.from(skillBoxes).forEach(function(box) {
+          box.style.left = '-999px';
+
+          // box to animate
+          if (box.classList.contains(skillName)) {
+            var boxWidth = box.offsetWidth;
+            var boxHeight = box.offsetHeight;
+
+            // set initial dimensions for animation
+            box.style.width = '0px';
+            box.style.height = '3px';
+            box.style.left = '50%';
+
+            animateBoxWidth(box, boxWidth).then(function() {
+              animateBoxHeight(box, boxHeight);
+            });
+          }
+        });
+
+        if (data.sound) {
+          zippi.play();
+        }
+      }
+    };
+  }, false);
+});
+
+function symmetry(svg) {
+  var width = svg.offsetWidth;
+  var svgDoc = svg.contentDocument;
+  var symmetrySource = svgDoc.getElementById('symmetry-source');
+  var symmetryTarget = svgDoc.getElementById('symmetry-target');
+
+  var elementsToFlip = symmetrySource.querySelectorAll('path, polygon, line, circle');
+
+  for (var i = 0; i < elementsToFlip.length; i++) {
+    var original = elementsToFlip[i];
+    var flipped = original.cloneNode(true);
+    if (original.getAttribute('symmetry-id')) {
+      flipped.setAttribute('id', original.getAttribute('symmetry-id'));
+    }
+
+    // flip line
+    if (flipped.tagName === 'line') {
+      var x1 = width - flipped.getAttribute('x1');
+      var x2 = width - flipped.getAttribute('x2');
+      flipped.setAttribute('x1', x1);
+      flipped.setAttribute('x2', x2);
+
+    // flip circle
+    } else if (flipped.tagName === 'circle') {
+      var cx = width - flipped.getAttribute('cx');
+      flipped.setAttribute('cx', cx);
+
+    // flip path, polygon
+    } else {
+      var attr;
+      if (flipped.tagName === 'path') {
+        attr = 'd';
+      } else if (flipped.tagName === 'polygon') {
+        attr = 'points';
+      }
+
+      var points = flipped.getAttribute(attr).split(' ');
+      for (var j = 0; j < points.length; j++) {
+        var x = points[j].match(/(\d+),/);
+        if (x) {
+          x = x[1];
+        } else {
+          continue;
+        }
+        x = width - x;
+        points[j] = points[j].replace(/\d+,/, x + ',');
+      }
+      points = points.join(' ');
+      flipped.setAttribute(attr, points);
+    }
+
+    symmetryTarget.appendChild(flipped);
+  }
+}
+
+function animateBoxWidth(box, boxWidth) {
+  return new Promise(function(resolve) {
+    var interval = setInterval(function() {
+      var width = parseInt(box.style.width, 10);
+
+      if (width >= boxWidth) {
+        clearInterval(interval);
+        resolve();
+        return;
+      }
+
+      box.style.width = (width + 10) + 'px';
+    }, 1);
+  });
+}
+
+function animateBoxHeight(box, boxHeight) {
+  var interval = setInterval(function() {
+    var height = parseInt(box.style.height, 10);
+
+    if (height >= boxHeight) {
+      clearInterval(interval);
       return;
     }
-    var index = parseInt(Math.random() * brainParts.length, 10);
-    $(brainParts[index]).addClass('active');
-    setTimeout(function() {
-      $(brainParts[index]).removeClass('active');
-      highlightBrainParts(depth + 1);
-    }, 50);
-  };
 
-  highlightBrainParts(0);
-});
+    box.style.height = (height + 10) + 'px';
+  }, 1);
+}
 
 console.log(`        ..
       ..  ..
