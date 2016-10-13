a951c0fdc6cf3215fbb07c6375d594de17a8034c
Limon Monte <limon.monte@gmail.com>
2016-10-14 01:06:58 +0200

    Upgrade to Vue.js 2

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
-'use strict';
+/* global Audio, Vue, TinyAnimate, Trianglify, localStorage */
+'use strict'
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   var data = {
-    sound: localStorage.getItem('sound')? JSON.parse(localStorage.getItem('sound')) : true,
-    backgroundMusic: new Audio('/mp3/The_Commission_Creton.mp3')
-  };
+    sound: localStorage.getItem('sound') ? JSON.parse(localStorage.getItem('sound')) : true,
+    backgroundMusic: new Audio('/mp3/The_Commission_Creton.mp3'),
+    sweetalert2: {}
+  }
 
   var vm = new Vue({
     el: '#app',
@@ -15,10 +17,11 @@ document.addEventListener('DOMContentLoaded', function() {
           name: {
             type: String
           },
+          href: {
+            type: String
+          },
           level: {
-            coerce: function(val) {
-              return parseInt(val, 10);
-            }
+            type: Number
           },
           inactive: {
             type: Boolean,
@@ -29,90 +32,90 @@ document.addEventListener('DOMContentLoaded', function() {
           '<span>' +
             '<pre v-if="level">[' +
               '<span v-for="n in level">=</span>' +
-              '<i v-if="!inactive">=</i><span v-else> </span>' +
-              '<span v-for="n in (19-level)"> </span>' +
+              '<i v-if="!inactive">=</i><span v-else>&nbsp;</span>' +
+              '<span v-for="n in (19-level)">&nbsp;</span>' +
             ']</pre>' +
-            '{{ name }}'+
+            '<a v-if="href" :href="href" v-html="name"></a><i v-else v-html="name"></i>' +
           '</span>'
       },
       'icon': {
         props: ['type'],
-        template: '<i class="fa fa-{{ type }}"></i>'
+        template: '<i v-bind:class="\'fa fa-\' + type"></i>'
       }
     },
     methods: {
-      fetchData: function() {
-        this.$http.get('https://api.github.com/repos/limonte/sweetalert2').then(function(response) {
-          this.$set('sweetalert2', response.data);
-        });
+      fetchData: function () {
+        this.$http.get('https://api.github.com/repos/limonte/sweetalert2').then(function (response) {
+          this.sweetalert2 = response.data
+        })
       }
     },
-    created: function() {
-      this.backgroundMusic.volume = 0.5;
-      this.backgroundMusic.loop = true;
+    created: function () {
+      this.backgroundMusic.volume = 0.5
+      this.backgroundMusic.loop = true
       if (this.sound) {
-        this.backgroundMusic.play();
+        this.backgroundMusic.play()
       }
-      this.fetchData();
+      this.fetchData()
     }
-  });
+  })
 
-  vm.$watch('sound', function(sound) {
+  vm.$watch('sound', function (sound) {
     if (sound) {
-      this.backgroundMusic.play();
+      this.backgroundMusic.play()
     } else {
-      this.backgroundMusic.pause();
+      this.backgroundMusic.pause()
     }
-    localStorage.setItem('sound', sound);
-  });
+    localStorage.setItem('sound', sound)
+  })
 
-  var zippi = new Audio('/mp3/zippi.mp3');
-  zippi.volume = 0.3;
+  var zippi = new Audio('/mp3/zippi.mp3')
+  zippi.volume = 0.3
 
-  var skillBoxes = document.getElementsByClassName('skillBox');
+  var skillBoxes = document.getElementsByClassName('skillBox')
 
-  var svg = document.getElementById('svg');
-  svg.addEventListener('load', function() {
-    symmetry(svg);
-    var svgDoc = svg.contentDocument;
+  var svg = document.getElementById('svg')
+  svg.addEventListener('load', function () {
+    symmetry(svg)
+    var svgDoc = svg.contentDocument
 
-    svgDoc.onclick = function(e) {
-      var target = e.target;
+    svgDoc.onclick = function (e) {
+      var target = e.target
       if (target.classList.contains('skill') && !target.classList.contains('current')) {
-        var current = svgDoc.getElementsByClassName('current');
+        var current = svgDoc.getElementsByClassName('current')
         if (current.length) {
-          current[0].classList.remove('current');
+          current[0].classList.remove('current')
         }
 
-        target.classList.add('active');
-        target.classList.add('current');
+        target.classList.add('active')
+        target.classList.add('current')
 
-        var skillName = target.getAttribute('id');
-        Array.from(skillBoxes).forEach(function(box) {
-          box.style.left = '-999px';
+        var skillName = target.getAttribute('id')
+        Array.from(skillBoxes).forEach(function (box) {
+          box.style.left = '-999px'
 
           // box to animate
           if (box.classList.contains(skillName)) {
-            var boxWidth = box.offsetWidth;
-            var boxHeight = box.offsetHeight;
+            var boxWidth = box.offsetWidth
+            var boxHeight = box.offsetHeight
 
             // set initial dimensions for animation
-            box.style.width = '0px';
-            box.style.height = '3px';
-            box.style.left = '50%';
+            box.style.width = '0px'
+            box.style.height = '3px'
+            box.style.left = '50%'
 
-            TinyAnimate.animateCSS(box, 'width', 'px', 0, boxWidth, 220, 'easeOutQuad', function() {
-              TinyAnimate.animateCSS(box, 'height', 'px', 3, boxHeight, 220, 'easeOutQuad');
-            });
+            TinyAnimate.animateCSS(box, 'width', 'px', 0, boxWidth, 220, 'easeOutQuad', function () {
+              TinyAnimate.animateCSS(box, 'height', 'px', 3, boxHeight, 220, 'easeOutQuad')
+            })
           }
-        });
+        })
 
         if (data.sound) {
-          zippi.play();
+          zippi.play()
         }
       }
-    };
-  }, false);
+    }
+  }, false)
 
   var pattern = Trianglify({
     width: window.innerWidth,
@@ -120,64 +123,64 @@ document.addEventListener('DOMContentLoaded', function() {
     cell_size: 100,
     x_colors: ['#0B2631', '#000000', '#000000', '#010F1C'],
     y_colors: ['#0B2631', '#000000', '#010F1C']
-  });
-  var trianglifyCanvas = pattern.canvas();
-  trianglifyCanvas.id = 'trianglify';
-  document.body.insertBefore(trianglifyCanvas, document.body.firstChild);
-});
+  })
+  var trianglifyCanvas = pattern.canvas()
+  trianglifyCanvas.id = 'trianglify'
+  document.body.insertBefore(trianglifyCanvas, document.body.firstChild)
+})
 
-function symmetry(svg) {
-  var width = svg.offsetWidth;
-  var svgDoc = svg.contentDocument;
-  var symmetrySource = svgDoc.getElementById('symmetry-source');
-  var symmetryTarget = svgDoc.getElementById('symmetry-target');
+function symmetry (svg) {
+  var width = svg.offsetWidth
+  var svgDoc = svg.contentDocument
+  var symmetrySource = svgDoc.getElementById('symmetry-source')
+  var symmetryTarget = svgDoc.getElementById('symmetry-target')
 
-  var elementsToFlip = symmetrySource.querySelectorAll('path, polygon, line, circle');
+  var elementsToFlip = symmetrySource.querySelectorAll('path, polygon, line, circle')
 
   for (var i = 0; i < elementsToFlip.length; i++) {
-    var original = elementsToFlip[i];
-    var flipped = original.cloneNode(true);
+    var original = elementsToFlip[i]
+    var flipped = original.cloneNode(true)
     if (original.getAttribute('symmetry-id')) {
-      flipped.setAttribute('id', original.getAttribute('symmetry-id'));
+      flipped.setAttribute('id', original.getAttribute('symmetry-id'))
     }
 
     // flip line
     if (flipped.tagName === 'line') {
-      var x1 = width - flipped.getAttribute('x1');
-      var x2 = width - flipped.getAttribute('x2');
-      flipped.setAttribute('x1', x1);
-      flipped.setAttribute('x2', x2);
+      var x1 = width - flipped.getAttribute('x1')
+      var x2 = width - flipped.getAttribute('x2')
+      flipped.setAttribute('x1', x1)
+      flipped.setAttribute('x2', x2)
 
     // flip circle
     } else if (flipped.tagName === 'circle') {
-      var cx = width - flipped.getAttribute('cx');
-      flipped.setAttribute('cx', cx);
+      var cx = width - flipped.getAttribute('cx')
+      flipped.setAttribute('cx', cx)
 
     // flip path, polygon
     } else {
-      var attr;
+      var attr
       if (flipped.tagName === 'path') {
-        attr = 'd';
+        attr = 'd'
       } else if (flipped.tagName === 'polygon') {
-        attr = 'points';
+        attr = 'points'
       }
 
-      var points = flipped.getAttribute(attr).split(' ');
+      var points = flipped.getAttribute(attr).split(' ')
       for (var j = 0; j < points.length; j++) {
-        var x = points[j].match(/(\d+),/);
+        var x = points[j].match(/(\d+),/)
         if (x) {
-          x = x[1];
+          x = x[1]
         } else {
-          continue;
+          continue
         }
-        x = width - x;
-        points[j] = points[j].replace(/\d+,/, x + ',');
+        x = width - x
+        points[j] = points[j].replace(/\d+,/, x + ',')
       }
-      points = points.join(' ');
-      flipped.setAttribute(attr, points);
+      points = points.join(' ')
+      flipped.setAttribute(attr, points)
     }
 
-    symmetryTarget.appendChild(flipped);
+    symmetryTarget.appendChild(flipped)
   }
 }
 
@@ -195,4 +198,4 @@ console.log(`        ..
 ##....WARM....###
  ##...........##
   ############
-`);
+`)
