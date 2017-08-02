4bb8e7479615faabc0a2ec0db198fe6ed51e5d40
Limon Monte <limon.monte@gmail.com>
2017-08-02 23:08:51 +0300

    Clean up service worker

--- sw.js
+++ sw.js
@@ -5,26 +5,20 @@ self.addEventListener('install', e => {
     caches.open('limonte').then(cache => {
       return cache.addAll([
         '/'
-        // '/css/styles.css',
-        // '/css/styles-svg.css',
-        // '/js/app.js',
-        // '/svg/head.svg.xml',
-        // '/mp3/zippi.mp3',
-        // '/mp3/The_Commission_Creton.mp3'
       ])
       .then(() => self.skipWaiting())
     })
   )
 })
 
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim())
+self.addEventListener('activate', e => {
+  e.waitUntil(self.clients.claim())
 })
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request)
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request)
     })
   )
 })
